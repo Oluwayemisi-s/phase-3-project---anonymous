c648281c4059ce78dfb5749042e9dee658fab721
garrettdick <garrettd0511@gmail.com>
2021-11-17 15:00:54 -0600

    completed conditional rendering for components using state

--- my-app-frontend/src/MovieCard.js
+++ my-app-frontend/src/MovieCard.js
@@ -1,11 +1,17 @@
-import MovieDetail from "./MovieDetail";
 
-function MovieCard({movie}){
+
+function MovieCard({movie, setShow, setMovie, setAppear, setHideIt}){
 
     let img_src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv${movie.thumbnail}`
 
+    function handleMovieClick(){
+        setMovie(movie)
+        setShow((cats) => !cats)
+        setHideIt((dogs)=> !dogs)
+    }
+
     return (
-        <div className = "MovieCard">
+        <div onClick={handleMovieClick} className = "MovieCard">
              <img src = {img_src} alt = "thumbnail"/>
             <h3>{movie.title}</h3>
             {/* <p>{movie.description}</p> */}
@@ -13,5 +19,4 @@ function MovieCard({movie}){
         </div>
     )
 }
-//asdas
 export default MovieCard;
--- my-app-frontend/src/SuggestedContainer.js
+++ my-app-frontend/src/SuggestedContainer.js
@@ -1,10 +1,10 @@
 import Suggested from "./MovieDetail";
 import MovieCard from "./MovieCard";
 
-function SuggestedContainer({suggested}){
+function SuggestedContainer({suggested, setMovie, setShow,setAppear, setHideIt}){
     // limit the suggested movies to about 10 - This will be handled in Ruby
     const suggested_movies = suggested.map( movie => 
-        <MovieCard key={movie.id} movie={movie}/>
+        <MovieCard key={movie.id} movie={movie} setShow={setShow} setMovie={setMovie} setAppear={setAppear} setHideIt={setHideIt}/>
     )
     return (
         <div>
